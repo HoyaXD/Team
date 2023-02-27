1a8068628feb1fd0507dafb3562c35f681d8b1ab
kbs <kbs0439@gmail.com>
2023-02-27 14:55:29 +0900

    0227 - 김범수 / cntPlus 수정

--- seenema/src/main/java/org/green/seenema/mapper/ReservationMapper.java
+++ seenema/src/main/java/org/green/seenema/mapper/ReservationMapper.java
@@ -20,7 +20,7 @@ public interface ReservationMapper {
 
     public List<String> loadSeats(ReservationVO reservation);
 
-    public void cntPlus(int movieCode, int visitors);
+    public void cntPlus(@Param("movieCode") int movieCode, @Param("visitors") int visitors);
 
     public TheaterVO loadTheater (String theaterPlace, String theaterName);
 
